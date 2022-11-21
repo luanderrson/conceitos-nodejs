6d72e70566af3c4efaffc6c9f77b336d708c9cc0
luanderrson <luandersonbms@gmail.com>
2022-11-21 20:39:46 -0300

    Desafios realizados

--- src/index.js
+++ src/index.js
@@ -1,34 +1,81 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username }  = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(400).json({ error: "User not Found" })
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  })
+
+  return response.status(201).send();
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.get('/todos/', checksExistsUserAccount, (request, response) => {
+ const { user } = request;
+
+ return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+
+  const deadLine = new Date(deadline + " 00:00");
+  
+  const { user } = request;
+
+  const insertToDo = {
+    id: uuidv4(),
+    title,
+    deadLine,
+    created_at: new Date()
+  }
+
+  user.todos.push(insertToDo);
+
+  return response.status(201).send();
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  user.todos.title[request.params.id]= title;
+  user.todos.deadLine[request.params.id]= deadline;
+
+  return response.status(201).send();
 });
+
+app.get('/users', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  return response.json(user);
+})
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   // Complete aqui
@@ -37,5 +84,6 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
+
 
 module.exports = app;
